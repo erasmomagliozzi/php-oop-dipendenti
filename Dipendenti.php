6c53b09fa4528163505889e99f776cb2020ebc82
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-03-11 15:37:36 +0100

    push

---
+++ Dipendenti.php
@@ -0,0 +1,21 @@
+<!-- azienda con dipendenti -->
+Dipendente
+ private id
+ private Nome
+ private Cognome
+ private LuogoNascita
+ private DataNascita
+ private Residenza
+ getId()
+ setId(id)
+ getNome()
+ setNome(Nome)
+ getCognome()
+ setNome(Cognome)
+
+
+Stipendio Dipendente
+ private Stipendio
+ private DataAccredito
+ private MetodoPagamento
+ private Iban
